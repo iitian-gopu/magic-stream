35975f9587f343b6af1ba86423bd6c7304b13f6a
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-21 12:52:11 +0530

    Extend main (Go)

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -10,3 +10,14 @@ import (
 
 	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/database"
 	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func main() {
+	// This is the main function
+
+	router := gin.Default()
+
