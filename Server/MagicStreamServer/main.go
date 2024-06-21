698471e81ab995d46934454e76f87441f5202195
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-21 11:36:21 +0530

    Scaffold main (Go)

---
+++ Server/MagicStreamServer/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/database"
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/routes"
