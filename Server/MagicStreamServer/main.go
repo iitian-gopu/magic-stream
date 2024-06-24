077949f2c205d4af85b9519a8f43aba020a3992c
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-24 19:24:47 +0530

    Improve main (Go)

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -32,3 +32,15 @@ func main() {
 
 	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
 
+	var origins []string
+	if allowedOrigins != "" {
+		origins = strings.Split(allowedOrigins, ",")
+		for i := range origins {
+			origins[i] = strings.TrimSpace(origins[i])
+			log.Println("Allowed Origin:", origins[i])
+		}
+	} else {
+		origins = []string{"http://localhost:5173"}
+		log.Println("Allowed Origin: http://localhost:5173")
+	}
+
