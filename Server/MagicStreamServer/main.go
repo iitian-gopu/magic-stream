715dc71df1bdc18d1063bbaf640a3ffc5eb1cb42
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-25 15:16:59 +0530

    Improve main (Go)

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -55,3 +55,14 @@ func main() {
 
 	router.Use(cors.New(config))
 	router.Use(gin.Logger())
+
+	var client *mongo.Client = database.Connect()
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		log.Fatalf("Failed to reach server: %v", err)
+	}
+	defer func() {
+		err := client.Disconnect(context.Background())
+		if err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
