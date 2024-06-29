96175753d70ffd0bbccd93f89695f6bceb6e7342
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-29 18:01:19 +0530

    Set up movieModel (Go)

---
+++ Server/MagicStreamServer/models/movieModel.go
@@ -0,0 +1,9 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Genre struct {
+	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
+	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
