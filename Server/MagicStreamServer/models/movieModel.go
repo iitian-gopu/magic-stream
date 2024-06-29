8a67e3c413de795fb5a8421b4eff9c39f5b42f94
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-06-29 20:08:38 +0530

    Refine movieModel (Go)

--- Server/MagicStreamServer/models/movieModel.go
+++ Server/MagicStreamServer/models/movieModel.go
@@ -7,3 +7,12 @@ import (
 type Genre struct {
 	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
 	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
+}
+
+type Ranking struct {
+	RankingValue int    `bson:"ranking_value" json:"ranking_value" validate:"required"`
+	RankingName  string `bson:"ranking_name" json:"ranking_name" validate:"required"`
+}
+
+type Movie struct {
+	ID          bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
