f050a426a78088b1e276896b94062404c58a2ccf
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-01 16:03:55 +0530

    Finalize userModel (Go)

--- Server/MagicStreamServer/models/userModel.go
+++ Server/MagicStreamServer/models/userModel.go
@@ -22,3 +22,15 @@ type User struct {
 }
 type UserLogin struct {
 	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+type UserResponse struct {
+	UserId          string  `json:"user_id"`
+	FirstName       string  `json:"first_name"`
+	LastName        string  `json:"last_name"`
+	Email           string  `json:"email"`
+	Role            string  `json:"role"`
+	Token           string  `json:"token"`
+	RefreshToken    string  `json:"refresh_token"`
+	FavouriteGenres []Genre `json:"favourite_genres"`
+}
