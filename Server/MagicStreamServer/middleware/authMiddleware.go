395db7e6c4d2409513a3d3aade250212844a877d
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-03 11:21:45 +0530

    Finalize authMiddleware (Go)

--- Server/MagicStreamServer/middleware/authMiddleware.go
+++ Server/MagicStreamServer/middleware/authMiddleware.go
@@ -26,3 +26,12 @@ func AuthMiddleWare() gin.HandlerFunc {
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 			c.Abort()
+			return
+		}
+		c.Set("userId", claims.UserId)
+		c.Set("role", claims.Role)
+
+		c.Next()
+
+	}
+}
