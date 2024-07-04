c4ebf8d52d8bbdd5b0c9178f469320061555e9f5
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-04 15:20:38 +0530

    Iterate on tokenUtil (Go)

--- Server/MagicStreamServer/utils/tokenUtil.go
+++ Server/MagicStreamServer/utils/tokenUtil.go
@@ -18,3 +18,22 @@ type SignedDetails struct {
 	FirstName string
 	LastName  string
 	Role      string
+	UserId    string
+	jwt.RegisteredClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+var SECRET_REFRESH_KEY string = os.Getenv("SECRET_REFRESH_KEY")
+
+func GenerateAllTokens(email, firstName, lastName, role, userId string) (string, string, error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		UserId:    userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "MagicStream",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
