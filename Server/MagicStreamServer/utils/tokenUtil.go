5ff592a782147742655c155538e8e12a6d4d5343
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-06 15:28:04 +0530

    Flesh out tokenUtil (Go)

--- Server/MagicStreamServer/utils/tokenUtil.go
+++ Server/MagicStreamServer/utils/tokenUtil.go
@@ -94,3 +94,22 @@ func UpdateAllTokens(userId, token, refreshToken string, client *mongo.Client) (
 
 func GetAccessToken(c *gin.Context) (string, error) {
 	// authHeader := c.Request.Header.Get("Authorization")
+	// if authHeader == "" {
+	// 	return "", errors.New("Authorization header is required")
+	// }
+	// tokenString := authHeader[len("Bearer "):]
+
+	// if tokenString == "" {
+	// 	return "", errors.New("Bearer token is required")
+	// }
+	tokenString, err := c.Cookie("access_token")
+	if err != nil {
+
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+func ValidateToken(tokenString string) (*SignedDetails, error) {
