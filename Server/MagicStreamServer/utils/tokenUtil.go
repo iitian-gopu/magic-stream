3e6fe93e9759f455058d68aaaeef11a3c8ed9f42
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-09 13:46:42 +0530

    Finish tokenUtil (Go)

--- Server/MagicStreamServer/utils/tokenUtil.go
+++ Server/MagicStreamServer/utils/tokenUtil.go
@@ -170,3 +170,22 @@ func GetRoleFromContext(c *gin.Context) (string, error) {
 
 func ValidateRefreshToken(tokenString string) (*SignedDetails, error) {
 	claims := &SignedDetails{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+
+		return []byte(SECRET_REFRESH_KEY), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, err
+	}
+
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return nil, errors.New("refresh token has expired")
+	}
+
+	return claims, nil
+}
