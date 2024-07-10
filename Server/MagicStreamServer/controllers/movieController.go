63255b44a0e773ae41ed068b22c507ba9e8fc698
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-10 21:28:56 +0530

    Flesh out movieController (Go)

--- Server/MagicStreamServer/controllers/movieController.go
+++ Server/MagicStreamServer/controllers/movieController.go
@@ -38,3 +38,42 @@ func GetMovies(client *mongo.Client) gin.HandlerFunc {
 		}
 		defer cursor.Close(ctx)
 
+		var movies []models.Movie
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies."})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
+
+	}
+}
+
+func GetMovie(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var movieCollection *mongo.Collection = database.OpenCollection("movies", client)
+
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.D{{Key: "imdb_id", Value: movieID}}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movie)
+
+	}
+}
