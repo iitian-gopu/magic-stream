52c9e8181023d222f14597882d2e707d0e5bdb5a
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-16 12:06:40 +0530

    Add userController (Go)

---
+++ Server/MagicStreamServer/controllers/userController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/database"
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/models"
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(HashPassword), nil
+
+}
