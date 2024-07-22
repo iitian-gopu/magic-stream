ddb34bf22c2c2e076878c2056e03a7a713c19d40
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-22 17:20:57 +0530

    Set up protectedRoutes (Go)

---
+++ Server/MagicStreamServer/routes/protectedRoutes.go
@@ -0,0 +1,9 @@
+package routes
+
+import (
+	controller "github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/controllers"
+	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/middleware"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
