be8c22ceab7fffeb913125eb6281e76710e0136a
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-22 19:21:57 +0530

    Finish protectedRoutes (Go)

--- Server/MagicStreamServer/routes/protectedRoutes.go
+++ Server/MagicStreamServer/routes/protectedRoutes.go
@@ -7,3 +7,11 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
+func SetupProtectedRoutes(router *gin.Engine, client *mongo.Client) {
+	router.Use(middleware.AuthMiddleWare())
+
+	router.GET("/movie/:imdb_id", controller.GetMovie(client))
+	router.POST("/addmovie", controller.AddMovie(client))
+	router.GET("/recommendedmovies", controller.GetRecommendedMovies(client))
+	router.PATCH("/updatereview/:imdb_id", controller.AdminReviewUpdate(client))
+}
