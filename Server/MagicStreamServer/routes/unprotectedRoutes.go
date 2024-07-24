3ee1a05a96c141298b5b7b8382c4276778c95090
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-07-24 19:39:29 +0530

    Finish unprotectedRoutes (Go)

--- Server/MagicStreamServer/routes/unprotectedRoutes.go
+++ Server/MagicStreamServer/routes/unprotectedRoutes.go
@@ -7,3 +7,11 @@ import (
 )
 
 func SetupUnProtectedRoutes(router *gin.Engine, client *mongo.Client) {
+
+	router.GET("/movies", controller.GetMovies(client))
+	router.POST("/register", controller.RegisterUser(client))
+	router.POST("/login", controller.LoginUser(client))
+	router.POST("/logout", controller.LogoutHandler(client))
+	router.GET("/genres", controller.GetGenres(client))
+	router.POST("/refresh", controller.RefreshTokenHandler(client))
+}
