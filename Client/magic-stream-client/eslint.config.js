87ea2e3e380d601c92d0251dba9a6a4eaa537894
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-08-12 13:07:02 +0530

    Scaffold eslint.config

---
+++ Client/magic-stream-client/eslint.config.js
@@ -0,0 +1,11 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
