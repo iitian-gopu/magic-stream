e6d25c4ac8f371002fc1d4cd4019ca61d39b5662
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-08-14 09:12:47 +0530

    Finalize eslint.config

--- Client/magic-stream-client/eslint.config.js
+++ Client/magic-stream-client/eslint.config.js
@@ -20,3 +20,14 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
