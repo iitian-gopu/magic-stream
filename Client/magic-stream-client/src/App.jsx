702f946075b72f9ee1171c0ca85617808ffce5e2
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-08-23 11:37:16 +0530

    Complete App

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -46,3 +46,15 @@ function App() {
         <Route path="/" element={<Home updateMovieReview={updateMovieReview}/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route element = {<RequiredAuth/>}>
+            <Route path="/recommended" element={<Recommended/>}></Route>
+            <Route path="/review/:imdb_id" element={<Review/>}></Route>
+            <Route path="/stream/:yt_id" element={<StreamMovie/>}></Route>
+        </Route>
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
