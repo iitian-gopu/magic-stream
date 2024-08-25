e914178bf6b0b2bf9222e8fb48fdc7674d7964e8
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-08-25 17:16:37 +0530

    Create main

---
+++ Client/magic-stream-client/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
