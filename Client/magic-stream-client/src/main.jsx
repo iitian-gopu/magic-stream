fd06faff2ff6591d41824d4dcb132833294924a2
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-08-26 09:42:02 +0530

    Polish main

--- Client/magic-stream-client/src/main.jsx
+++ Client/magic-stream-client/src/main.jsx
@@ -8,3 +8,12 @@ import { AuthProvider } from './context/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/*" element ={<App/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
+)
