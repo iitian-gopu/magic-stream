d6a5e7ea95e5a4dc774a4ae8f6d412e87d131d23
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-09-14 19:01:30 +0530

    Improve Login component

--- Client/magic-stream-client/src/components/login/Login.jsx
+++ Client/magic-stream-client/src/components/login/Login.jsx
@@ -10,3 +10,15 @@ import logo from '../../assets/MagicStreamLogo.png';
 const Login = () => {
     
     const {setAuth} = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || "/";
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
