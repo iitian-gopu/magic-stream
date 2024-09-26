55d2cf360c38e9e87fd4937549e69f0b3acf88f9
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-09-26 11:16:23 +0530

    Scaffold Register component

---
+++ Client/magic-stream-client/src/components/register/Register.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect} from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axiosClient from '../../api/axiosConfig';
+import { useNavigate, Link } from 'react-router-dom';
+import logo from '../../assets/MagicStreamLogo.png';
+
+const Register = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [favouriteGenres, setFavouriteGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
