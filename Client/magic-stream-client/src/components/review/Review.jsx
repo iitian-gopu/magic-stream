702fad5c9de27696f7be051c18e786f351fe1e69
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-10-04 20:42:06 +0530

    Scaffold Review component

---
+++ Client/magic-stream-client/src/components/review/Review.jsx
@@ -0,0 +1,12 @@
+import {Form, Button} from 'react-bootstrap';
+import { useRef,useEffect,useState } from 'react';
+import {useParams} from 'react-router-dom'
+//import axiosPrivate from '../../api/axiosPrivateConfig';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import useAuth from '../../hooks/useAuth';
+import Movie from '../movie/Movie'
+import Spinner from '../spinner/Spinner';
+
+const Review = () => {
+
+    const [movie, setMovie] = useState({});
