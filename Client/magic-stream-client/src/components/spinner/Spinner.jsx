41749b480f1a2058c2c8b6382e6850a05415eddb
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2024-10-11 22:50:39 +0530

    Polish Spinner component

--- Client/magic-stream-client/src/components/spinner/Spinner.jsx
+++ Client/magic-stream-client/src/components/spinner/Spinner.jsx
@@ -9,3 +9,14 @@ const Spinner = () => {
             alignItems: 'center',
             minHeight: '60vh'
           }}>
+            <span
+              className="spinner-border"
+              role="status"
+              aria-hidden="true"
+              style={{ width: '5rem', height: '5rem', fontSize: '2rem' }}
+            ></span>
+          </div>
+    );
+}
+
+export default Spinner;
